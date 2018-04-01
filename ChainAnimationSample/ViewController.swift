2513d09cbdd0ab2f30b5948dce0a1f6670de4a0a
AliGorithm <alisher.coder@gmail.com>
2018-03-31 23:51:39 -0500

    simpleChainAnimation

--- ChainAnimationSample/ViewController.swift
+++ ChainAnimationSample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     // Set up UI
     let titleLabel = UILabel()
     let bodyLabel = UILabel()
+    var isShowing = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,17 @@ class ViewController: UIViewController {
         setUpLabels()
         setUpStackView()
         
+        // Animate
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc func handleTap() {
+        
+        animateView(uiview: titleLabel, delay: 0)
+        
+        // BodyLabel
+        
+        animateView(uiview: bodyLabel, delay: 0.5)
         
     }
 
@@ -44,6 +56,23 @@ class ViewController: UIViewController {
         
         bodyLabel.text = "Animations are clipped to the duration of the animation group. For example, a 10 second animation grouped within an animation group with a duration of 5 seconds will only display the first 5 seconds of the animation"
         bodyLabel.numberOfLines = 0
+    }
+    
+    
+    fileprivate func animateView(uiview: UIView, delay: Double) {
+        
+        UIView.animate(withDuration: 0.5, delay: TimeInterval(delay), usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            
+            uiview.transform = CGAffineTransform(translationX: -30, y: 0)
+            
+        }) {  (_) in
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                uiview.alpha = 0
+                uiview.transform = self.titleLabel.transform.translatedBy(x: 0, y: -200)
+                
+            }, completion: nil)
+        }
     }
 }
 
